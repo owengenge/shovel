ce09f92f384fd9850973b146396463f7a02c9f45
Owen Genge <owenxgenge@gmail.com>
2026-01-19 15:52:04 -0700

    Updating attack location buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,68 @@ import { sessionReducer, initialSessionState } from "./reducer/sessionReducer";
 
 function App() {
   const [state, dispatch] = useReducer(sessionReducer, initialSessionState);
+  const canSave = state.currentAttackLocation && state.currentContactZone;
+  
+  const ATTACK_LOCATIONS = [
+    { label: "RS", row: "FR" },
+    { label: "50", row: "FR" },
+    { label: "30", row: "FR" },
+    { label: "LS", row: "FR" },
+    { label: "C", row: "BR" },
+    { label: "PIPE", row: "BR" },
+  ];
+  
 
   return (
     <div>
-      <h1>Volleyball Defensive Stat Tracker</h1>
+      <h1>SHOVEL</h1>
+      <p>Volleyball Defensive Stat Tracker</p>
 
       <div>
-        <button onClick={() => dispatch({ type: "SET_ATTACK_LOCATION", payload: "RS" })}>
-          Right Side
+        <h3>Back Row</h3>
+        {ATTACK_LOCATIONS
+          .filter(loc => loc.row === "BR")
+          .map(loc => (
+            <button
+              key={loc.label}
+              onClick={() =>
+                dispatch({ type: "SET_ATTACK_LOCATION", payload: loc.label })
+              }
+            >
+              {loc.label}
+            </button>
+        ))}
+      </div>
+
+      <div>
+        <h3> Front Row </h3>
+        {ATTACK_LOCATIONS.filter(loc => loc.row === "FR").map(loc => (
+          <button
+            key={loc.label}
+            onClick={() => 
+              dispatch({ type: "SET_ATTACK_LOCATION", payload: loc.label})
+            }
+          >
+            {loc.label}
+          </button>
+        ))}
+      </div>
+
+      <div>
+        <button onClick={() => dispatch({ type: "SET_CONTACT_ZONE", payload: "Test Contact Zone" })}>
+          Contact Zone
         </button>
-        <button onClick={() => dispatch({ type: "SET_CONTACT_ZONE", payload: "DIRECT" })}>
-          Direct
-        </button>
-        <button onClick={() => dispatch({ type: "ADD_EVENT", payload: { success: true } })}>
+        
+        <button
+          disabled={!canSave}
+          onClick={() => dispatch({ type: "ADD_EVENT", payload: { success: true } })}
+        >
           Successful Dig
         </button>
-        <button onClick={() => dispatch({ type: "ADD_EVENT", payload: { success: false } })}>
+        <button
+          disabled={!canSave}
+          onClick={() => dispatch({ type: "ADD_EVENT", payload: { success: false } })}
+        >
           Unsuccessful Dig
         </button>
         <button onClick={() => dispatch({ type: "UNDO_PREV_EVENT" })}>
